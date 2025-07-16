c0c53dab38b2a8c8290d54daed3d327f5eb162be
Frontend-kik <153603429+Frontend-kik@users.noreply.github.com>
2025-07-16 17:08:13 +0300

    4. Обработка POST-запроса

--- 3-http/index.js
+++ 3-http/index.js
@@ -1,13 +1,25 @@
 // Создание сервера на Node.js
 const http = require("http");
 const chalk = require("chalk");
+const fs = require("fs/promises");
+const path = require("path");
 
 const port = 3000;
+// базовый путь к файлам
+const basePath = path.join(__dirname, "pages");
 // Создаем сервер
-const server = http.createServer((req, res) => {
-  console.log("request method", req.method);
-  console.log("request url", res.url);
-  res.end("hello from server!!!!qqqq");
+const server = http.createServer(async (req, res) => {
+  if (req.method === "GET") {
+    const content = await fs.readFile(path.join(basePath, "index.html"));
+    // res.setHeader("Content-Type", "text/html");
+    res.writeHead(
+      200,
+      (headers = {
+        "Content-Type": "text/html",
+      })
+    );
+    res.end(content);
+  }
 });
 
 // запускаем сервер на порту 3000 и вызываем метод listen callback
