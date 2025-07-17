c437b17515b2b37853ea7c673ce698504aedd907
Frontend-kik <153603429+Frontend-kik@users.noreply.github.com>
2025-07-17 11:29:41 +0300

    Express

--- 3-http/index.js
+++ 3-http/index.js
@@ -1,28 +1,27 @@
-// Создание сервера на Node.js
 const http = require("http");
+// подключаем express. server вынесен в отдельный файл
+const express = require("express");
 const chalk = require("chalk");
 const fs = require("fs/promises");
 const path = require("path");
+const { addNote } = require("./notes.controller");
 
 const port = 3000;
-// базовый путь к файлам
 const basePath = path.join(__dirname, "pages");
-// Создаем сервер
-const server = http.createServer(async (req, res) => {
-  if (req.method === "GET") {
-    const content = await fs.readFile(path.join(basePath, "index.html"));
-    // res.setHeader("Content-Type", "text/html");
-    res.writeHead(
-      200,
-      (headers = {
-        "Content-Type": "text/html",
-      })
-    );
-    res.end(content);
-  }
+// вызываем функцию экспресс
+const app = express();
+
+app.use(express.urlencoded({}));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(basePath, "index.html"));
 });
 
-// запускаем сервер на порту 3000 и вызываем метод listen callback
-server.listen(port, () => {
+app.post("/", async (req, res) => {
+  await addNote(req.body.title);
+  res.sendFile(path.join(basePath, "index.html"));
+});
+
+app.listen(port, () => {
   console.log(chalk.green(`Server has been started ${port}...`));
 });
